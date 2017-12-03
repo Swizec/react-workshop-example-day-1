6d7461bddb796fc126ebe47ecd29bb01889d365c
Swizec Teller <swizec@swizec.com>
2017-12-02 16:25:44 -0800

    Show list of tickets

--- src/App.js
+++ src/App.js
@@ -4,10 +4,21 @@ import "./App.css";
 
 import data from "./data.json";
 import Ticket from "./Ticket";
+import TicketList from "./TicketList";
 
 class App extends Component {
+    state = {
+        N: 2
+    };
+
     render() {
-        return <Ticket info={data.events[0]} />;
+        const { N } = this.state;
+
+        return (
+            <TicketList tickets={data.events} N={N}>
+                {({ info }) => <Ticket info={info} key={info.id} />}
+            </TicketList>
+        );
     }
 }
 
---
+++ src/TicketList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const CenteredList = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 960px;
+    margin: 0 auto;
+`;
+
+const TicketList = ({ tickets, N, children }) => (
+    <CenteredList>
+        {tickets.slice(0, N).map(info => children({ info }))}
+    </CenteredList>
+);
+
+export default TicketList;
